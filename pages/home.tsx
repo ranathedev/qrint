50176b82f86e55d864d4af0a86e41383f8c0b7a5
ranaintizar <intizaralirana2@gmail.com>
2023-05-24 23:51:40 +0500

    Create Hero Section

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "components/header";
+import HeroSection from "components/hero-section";
 import Intro from "components/intro";
 
 import stl from "./index.module.scss";
@@ -10,6 +11,18 @@ const Homepage = () => {
     <div className={stl.wrapper}>
       <Header />
       <Intro />
+      <HeroSection />
+      <HeroSection
+        imgPosition="right"
+        heading="Generate Your Own Codes"
+        desc="Create personalized QR codes to share information and engage your audience. Generate codes for websites, text, contact information, and more. Tailor the content and design of your codes to suit your needs. Empower yourself to effortlessly generate codes and enhance communication."
+        imgSrc="/generate.png"
+      />
+      <HeroSection
+        heading="Customize Code Styles"
+        desc="Make your QR codes stand out and reflect your unique style. Customize the appearance of your codes with various styles, colors, and patterns. Add logos, brand colors, and custom designs to make your codes visually appealing and aligned with your brand identity. Create eye-catching codes that leave a lasting impression."
+        imgSrc="/styles.png"
+      />
     </div>
   );
 };

25f1e3815f26aee4c51e99488fbce0225f645fec
ranaintizar <intizaralirana2@gmail.com>
2023-05-29 00:28:23 +0500

    Create 404 Page

---
+++ pages/_error.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ErrorPage from "next/error";
+
+import PageNotFound from "./404";
+
+const CustomErrorPage = ({ statusCode }: { statusCode: number }) => {
+  if (statusCode === 404) {
+    return <PageNotFound />;
+  }
+
+  return <ErrorPage statusCode={statusCode} />;
+};
+
+export default CustomErrorPage;

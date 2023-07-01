71f876b425e84a05c5b49da28fa56a109cfb1993
Rana Intizar <110844434+ranaintizar@users.noreply.github.com>
2023-07-01 12:31:47 +0500

    Fix Styles for Small Screen (#86)
    * Fix Styles for Small Screen

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,9 +10,9 @@ const PageNotFound = () => {
         404: <span>Page Not Found</span>
       </h1>
       <p>
-        Oops! Looks like you've taken a wrong turn. This page seems to be lost
-        in cyberspace. Don't worry, it happens to the best of us. Let's get you
-        back on track.
+        Oops! Looks like you&apos;ve taken a wrong turn. This page seems to be
+        lost in cyberspace. Don&apos;t worry, it happens to the best of us.
+        Let&apos;s get you back on track.
       </p>
       <Link href="/">Go to Home</Link>
     </div>
